7ad24a459bad06e57424aad143e9992780254951
Douglas Henrique de Souza Pereira <50143403+douglazhs@users.noreply.github.com>
2022-05-19 17:51:47 -0300

    feat: dependency injection implemented

--- financeApp/financeApp/View/InitialView/InitialViewModel.swift
+++ financeApp/financeApp/View/InitialView/InitialViewModel.swift
@@ -11,56 +11,31 @@ import CoreData
 
 class InitialViewModel: NSObject, ObservableObject{
     @Published var budget: String = ""
-    @Published var remaining: Double = 0
     @Published var user: User?
+    @Published var spents: [Spent] = [Spent]()
+    var dataManager: DataFetchable
     
-    private var fetchedBudgetController: NSFetchedResultsController<User>
-    
-    init(context: NSManagedObjectContext){
-        fetchedBudgetController = NSFetchedResultsController(fetchRequest: User.all, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+    init(dataManager: DataFetchable){
+        self.dataManager = dataManager
         super.init()
-        fetchedBudgetController.delegate = self
         fetchBudget()
     }
     
     func fetchBudget(){
-        do{
-            try fetchedBudgetController.performFetch()
-            guard let user = fetchedBudgetController.fetchedObjects else{
-                return
+        dataManager.fetchUser { user in
+            withAnimation {
+                self.user = user
             }
-            guard let user = user.first else {
-                return
-            }
-            self.user = user
-            self.budget = String(format: "%.2f", user.budget)
-        }catch{
-            print(error.localizedDescription)
         }
     }
-    
-    func calculateBudget(){
-        
-    }
-    
-    func updateBudget(){
-        user?.budget = Double("\(budget)") ?? 0.0
-        do{
-            try user?.save()
-        }catch{
-            print(error.localizedDescription)
-        }
+
+    func hideKeyboard(){
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
 
 extension InitialViewModel: NSFetchedResultsControllerDelegate{
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        guard let user = controller.fetchedObjects?.first as? User else{
-            return
-        }
-        withAnimation {
-            self.user = user
-            self.budget = "\(user.budget)"
-        }
+        
     }
 }
--- financeApp/financeApp/View/WalletCard/WalletViewModel.swift
+++ financeApp/financeApp/View/WalletCard/WalletViewModel.swift
@@ -10,24 +10,62 @@ import CoreData
 
 class WalletViewModel: NSObject, ObservableObject{
     @Published var budget: String = ""
+    @Published var isEditing: Bool = true
+    @Published var showPopUp: Bool = false
     @Published var fetchedBudget: Double = 0
-    var formattedBudget: Double {
-        return (Double(budget) ?? 0) / 100
-    }
-    private var fetchedBudgetController: NSFetchedResultsController<User>
+    @Published var totalExpanses: Double = 0
+    @Published var remainingBudget: Double = 0
+    @Published var user: User?
+    var dataManager: DataFetchable
+    var spents: [Spent] = [Spent]()
     
-    init(context: NSManagedObjectContext){
-        fetchedBudgetController = NSFetchedResultsController(fetchRequest: User.all, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+    init(dataManager: DataFetchable){
+        self.dataManager = dataManager
         super.init()
-        fetchedBudgetController.delegate = self    }
+        fetchUser()
+        calculateTotalExpenses()
+    }
     
-    func updateBudget(for user: User?){
-        user?.budget = Double("\(budget)") ?? 0.0
+    func fetchUser(){
+        dataManager.fetchUser { user in
+            self.user = user
+            self.budget = String(format: "%.2f", user.budget)
+        }
+    }
+    
+    func calculateTotalExpenses(){
+        dataManager.fetchSpents { spents in
+            self.spents = spents
+        }
+        
+        for spent in spents{
+            self.totalExpanses += spent.cost
+        }
+        
+        guard let user = user else {
+            return
+        }
+        
+        self.remainingBudget = totalExpanses - user.budget
+    }
+    
+    func updateBudget(){
+        guard let user = user else {
+            return
+        }
+        user.budget = Double("\(budget)") ?? 0.0
         do{
-            try user?.save()
+            try user.save()
         }catch{
             print(error.localizedDescription)
         }
+    }
+    
+    /// Hide keyboard when click at screen
+    func hideKeyboard(){
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+        isEditing = false
+        showPopUp = true
     }
 }
 
